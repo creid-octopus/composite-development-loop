8bcc2deda02b45479a389cfae209e5c815dee5eb
Cory Reid <cory.reid@octopus.com>
2026-06-02 11:48:46 -0400

    Added keeper logic to random string to support CD runs

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,12 @@ provider "azurerm" {
 
 locals {
   name_base = "${var.resource_prefix}-${var.environment}"
+  # Enable cloud target discovery for Octopus
+  cloud_target_discovery_tags = {
+    "octopus-environment" = "Development"
+    "octopus-role"        = "development-loop"
+    "octopus-project"     = "Composite Development Loop"
+  }
 }
 
 # -- Random ID for uniqueness -──────────────────────────────────────────────────────────────
@@ -25,6 +31,10 @@ locals {
 resource "random_string" "suffix" {
   length  = 5
   special = false
+  # Ensure that we retain this between runs so that the web app name doesn't change on every apply
+  keepers = {
+    environment = var.environment
+  }
 }
 
 # ── Resource Group ──────────────────────────────────────────────────────────────
@@ -61,11 +71,12 @@ resource "azurerm_linux_web_app" "app" {
 
   app_settings = {
     # Populated by Octopus during deployment
-    APP_ENV         = var.environment
+    APP_ENV                  = var.environment
     WEBSITE_RUN_FROM_PACKAGE = "1"
   }
 
   https_only = true
+  tags       = local.cloud_target_discovery_tags
 }
 
 # ── Deployment Slot (feature-branch testing) ────────────────────────────────────
@@ -82,7 +93,7 @@ resource "azurerm_linux_web_app_slot" "feature" {
   }
 
   app_settings = {
-    APP_ENV         = "${var.environment}-feature"
+    APP_ENV                  = "${var.environment}-feature"
     WEBSITE_RUN_FROM_PACKAGE = "1"
   }
 
