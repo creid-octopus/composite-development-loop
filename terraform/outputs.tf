b056c3d68b1fe1cdd6c20fc500525faea574f2e4
Cory Reid <cory.reid@octopus.com>
2026-06-03 12:08:29 -0400

    revert

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
 output "webapp_configuration" {
   value = {
     development = {
-      resource_group_name = module.web_app_development.resource_group_name
-      app_name            = module.web_app_development.app_name
-      app_url             = module.web_app_development.app_url
-      slot_url            = module.web_app_development.feature_slot_url
+      resource_group_name = module.web_app_dev.resource_group_name
+      app_name            = module.web_app_dev.app_name
+      app_url             = module.web_app_dev.app_url
+      slot_url            = module.web_app_dev.feature_slot_url
     }
     test = {
       resource_group_name = module.web_app_test.resource_group_name
